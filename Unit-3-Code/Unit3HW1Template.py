bd553d5cc0f7f15a9bee6e61d2bbb1bb7f189e54
smithbwlhs <smithb@wlwv.k12.or.us>
2024-04-01 11:27:54 -0700

    new file

--- Unit-3-Code/Unit3HW1Template.py
+++ Unit-3-Code/Unit3HW1Template.py
@@ -1,11 +1,15 @@
+PI = 3.1415
+
 def sphere_volume(radius):
-    volume = 0 # modify this line
+    volume = 4 * PI * radius**3 / 3 # modify this line
     return volume # leave this
 
 def book_cost():
     return None
 
 def main():
+    # prompt the user for a radius and store in a variable
+
     print(sphere_volume(6)) # should get 904.752
 
 if __name__ == '__main__':
---
+++ Unit-3-Code/lessons_1-5_review.py
@@ -0,0 +1,23 @@
+'''
+Date: 4/1/2024
+Lesson: Units 1-5 Review
+'''
+
+def main():
+    # get pet breed
+    breed = input("What is your pet's breed? ")
+    
+    # get pet weight in lbs
+    weight = float(input("What is your pet's weight in pounds? "))
+   
+    # convert pet weight
+    lb_weight = weight * 2.2
+    
+    # create message to user
+    message = f"Your {breed} weighs {weight}kg or {lb_weight}lbs."
+   
+    # print message to user
+    print(message)
+
+if __name__ == '__main__':
+    main()
