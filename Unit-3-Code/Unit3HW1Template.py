861ab858920d3e7fda01a9835900df8289005a81
smithbwlhs <smithb@wlwv.k12.or.us>
2024-04-01 10:21:33 -0700

    typo fixed

--- Unit-3-Code/Unit3HW1Template.py
+++ Unit-3-Code/Unit3HW1Template.py
@@ -1,12 +1,12 @@
-def sphere_volume():
-    volume = 0
-    return volume
+def sphere_volume(radius):
+    volume = 0 # modify this line
+    return volume # leave this
 
-def book_cose():
+def book_cost():
     return None
 
 def main():
-    print(sphere_volume(6))
+    print(sphere_volume(6)) # should get 904.752
 
 if __name__ == '__main__':
     main()
