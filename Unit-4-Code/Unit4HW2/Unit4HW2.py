a91d7a1246139e167799023480ec71bbebbebca4
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2024-04-16 08:29:15 -0700

    Create Unit4HW2.py

---
+++ Unit-4-Code/Unit4HW2/Unit4HW2.py
@@ -0,0 +1,16 @@
+'''
+Name:
+Date:
+Assignment:
+'''
+
+def valid_triangle(side_one, side_two, side_three):
+    print()
+
+def main():
+    print()
+
+
+# don't change code below this
+if __name__ == "__main__":
+    main()
