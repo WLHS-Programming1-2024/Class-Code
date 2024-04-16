750c0fdfc8ddd50b3ff0c5c41eacda9af5ef3ba1
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2024-04-16 08:52:59 -0700

    Update Unit4HW2.py

--- Unit-4-Code/Unit4HW2/Unit4HW2.py
+++ Unit-4-Code/Unit4HW2/Unit4HW2.py
@@ -4,11 +4,22 @@ Date:
 Assignment:
 '''
 
+def number_classifier(number_one, number_two):
+    print()
+
 def valid_triangle(side_one, side_two, side_three):
     print()
 
 def main():
-    print()
+    # test number classifier with 5 test cases using positive, zero, negative numbers
+
+
+    # test valid triangle function with at least 5 test cases
+
+
+    # comment out the tests then do user input
+
+    
 
 
 # don't change code below this
