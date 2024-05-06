0113516c129c823565cdd27b9bf83630f7afbf6d
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2024-05-06 11:26:05 -0700

    Create droid.py

---
+++ Unit-6-Code/U6-L1/droid.py
@@ -0,0 +1,28 @@
+# function example for many units
+# functions let us break programs into 
+# small, easy to understand, easy to 
+# diagnose parts
+
+# no parameter function
+def print_menu():
+    print(f"Welcome to the droid interface. Right now, you can\
+ just create a droid, but there is more coming.")
+    print(r"""
+     ,     ,
+    (\____/)
+     (_oo_)
+       (O)
+     __||__    \)
+  []/______\[] /
+  / \______/ \/
+ /    /__\
+(\   /____\
+    
+    """)
+
+def create_droid(name,skill,battery_level):
+    print()
+
+
+def main():
+    print_menu()
