cc6469ef622dd47a037b1c57736bfd9ce062d155
smithbwlhs <smithb@wlwv.k12.or.us>
2024-05-22 10:51:17 -0700

    Update droid.py

---
+++ Unit-7-Code/droid.py
@@ -0,0 +1,40 @@
+# function example for many units
+# functions let us break programs into 
+# small, easy to understand, easy to 
+# diagnose parts
+
+# no parameter function
+def print_menu():
+    print(f"Welcome to the droid interface. Right now, you can\
+ just create a droid, but there is more coming.")
+    print(r"""
+     ,     ,
+    (\____/)
+     (_oo_)
+       (O)
+     __||__    \)
+  []/______\[] /
+  / \______/ \/
+ /    /__\
+(\   /____\
+    
+    """)
+
+def create_droid():
+    
+def droid_info(:
+    
+def quick_charge():
+    
+
+def time_charge():
+    
+
+
+def main():
+    print_menu()
+    # droid list format is name,skill,battery_level
+    droid_one_info = []
+
+if __name__ == '__main__':
+    main()
